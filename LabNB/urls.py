d77a5476f6b686b49d6c7fc57a2439691eebf63a
Parth Lakhani <parth.lakhani@ucsf.edu>
2015-03-12 03:31:43 -0700

    Add Django HTML5 boilerplate (dh5bp) to installed apps and urls.

--- LabNB/urls.py
+++ LabNB/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+# Added for Django HTML5 Boilerplate
+from dh5bp.urls import urlpatterns as dh5bp_urls
+
+handler404 = 'dh5bp.views.page_not_found'
+handler500 = 'dh5bp.views.server_error'
+
+# URLs
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'LabNB.views.home', name='home'),
@@ -8,3 +16,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# some more dh5bp defaults (favicon.ico, apple-touch-icon.png, humans.txt, robots.txt, and crossdomain.xml)
+urlpatterns += dh5bp_urls
